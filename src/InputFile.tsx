c3d6df77a38f53f7f529871690a0c7987069ebd5
Amelia Wattenberger <wattenberger@github.com>
2022-01-31 20:45:27 -0500

    feat: add support for other dsv delimiters

--- src/InputFile.tsx
+++ src/InputFile.tsx
@@ -5,32 +5,39 @@ import * as d3 from "d3"
 import icons from "./icons"
 
 type InputFileProps = {
-    fileName: String,
-    onChange: (data: any) => any,
+  fileName: String,
+  onChange: (data: any) => any,
 }
 const InputFile: React.FunctionComponent<InputFileProps> = ({
-    fileName,
-    onChange,
+  fileName,
+  onChange,
 }) => {
-    const onFileUpload = async e => {
+  const onFileUpload = async e => {
+    try {
+      const file = e.target.files[0];
+      const contents: string = await readUploadedFileAsText(file)
       try {
-        const file = e.target.files[0];
-        const contents: string = await readUploadedFileAsText(file)
+        onChange({
+          data: JSON.parse(contents),
+          file,
+        })
+      } catch (e) {
         try {
           onChange({
-              data: JSON.parse(contents),
-              file,
+            data: d3.csvParse(contents),
+            file,
           })
-        } catch(e) {
+        } catch (e) {
           onChange({
-              data: d3.csvParse(contents),
-              file,
+            data: d3.tsvParse(contents),
+            file,
           })
         }
-      } catch(e) {
-        throw Error(e)
       }
+    } catch (e) {
+      throw Error(e)
     }
+  }
   return (
     <div className={`inputfile is-${!!fileName ? "selected" : "not-selected"}`}>
       <input
@@ -41,8 +48,8 @@ const InputFile: React.FunctionComponent<InputFileProps> = ({
         onChange={onFileUpload}
       />
       <label htmlFor="data-upload">
-        { icons.upload }
-        { fileName || "Or feed me a CSV or JSON file" }
+        {icons.upload}
+        {fileName || "Or feed me a CSV or JSON file"}
       </label>
     </div>
   )
@@ -50,7 +57,7 @@ const InputFile: React.FunctionComponent<InputFileProps> = ({
 
 export default InputFile
 
-const readUploadedFileAsText = function(inputFile: any): any {
+const readUploadedFileAsText = function (inputFile: any): any {
   const temporaryFileReader = new FileReader()
 
   return new Promise((resolve, reject) => {
